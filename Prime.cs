225dff6d853ca3e50b511af413d659f4d7999180
sanikaghule16 <sanikaghule4@gmail.com>
2026-04-08 11:39:39 +0530

    Add program to print even numbers from 2 to 100

---
+++ Prime.cs
@@ -0,0 +1,13 @@
+namespace ConsoleApp3
+{
+
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 2; i <= 100; i += 2)
+                Console.Write(i + " ");
+        }
+    }
+}
